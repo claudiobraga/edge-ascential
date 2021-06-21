27e5767450c1d910c8a4721b76871f45627b405b
claudiobraga <cjbdeveloper@gmaiil.com>
2021-06-21 03:50:21 +0100

    initial commit

---
+++ JavaTechnicalTest/src/main/java/com/edge/ascential/claudio/braga/JavaTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.edge.ascential.claudio.braga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTechnicalTestApplication.class, args);
+	}
+
+}
---
+++ JavaTechnicalTest/src/test/java/com/edge/ascential/claudio/braga/JavaTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edge.ascential.claudio.braga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
